40415f82012e78b5222c103daf3340ca11e0fe84
mounikapavani <mounipavani.02@gmail.com>
2026-04-02 23:30:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ if user_query:
     else:
         try:
             genai.configure(api_key=api_key)
-            model = genai.GenerativeModel("gemini-1.5-flash")
+            model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
             prompt = f"""
 You are an MSME financial helper in Telangana.
